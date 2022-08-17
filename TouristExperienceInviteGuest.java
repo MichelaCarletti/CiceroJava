00f1274323f1d209863de0c7ab9e3bd3bead8614
MichelaCarletti <81373725+MichelaCarletti@users.noreply.github.com>
2022-08-17 17:25:08 +0200

    Delete TouristExperienceInviteGuest.java

--- TouristExperienceInviteGuest.java
+++
@@ -1,8 +0,0 @@
-public class TouristExperienceInviteGuest {
-
-    private int tourist_experience_invite_guest_id;
-    private int tourist_id;
-    private int tour_id;
-    private String invite_day;
-    private String guest_email;
-}
